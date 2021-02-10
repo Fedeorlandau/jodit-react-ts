4229e1a8e37653b808964a835ee47c361c53a9ed
Federico Orlandau <federicoorlandau@gmail.com>
2021-02-10 20:52:18 -0300

    Fix example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { JoditReact } from '../src/index';
+import JoditReact from '../src/index';
 
 const App = () => {
   const [value, setValue] = React.useState<string>();
